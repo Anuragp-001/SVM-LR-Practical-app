67c3e7a9ce7edb6271d412a3d8a6d409d26a2099
air73 <air735599@gmail.com>
2025-06-17 16:26:19 +0530

    mongodb url fixing

--- Svm_MultiApp.py
+++ Svm_MultiApp.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import StandardScaler
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://Anurag:<db_password>@cluster0.bagh3cm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://Anurag:Anurag1234@cluster0.bagh3cm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client["Iris"]
--- mongodb_connect.py
+++ mongodb_connect.py
@@ -2,7 +2,8 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://Anurag:<db_password>@cluster0.bagh3cm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = uri = "mongodb+srv://Anurag:Anurag1234@cluster0.bagh3cm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
